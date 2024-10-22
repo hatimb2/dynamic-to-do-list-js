716a1f77917efea9937b880570b575d58abc52b9
hatimbr <hatim-boutejdir@hotmail.com>
2024-10-22 14:45:15 +0100

    done

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Select DOM Elements
-    const addButton = document.getElementById('add-task-btn');
+    const addButton = document.getElementById('classList.add');
     const taskInput = document.getElementById('task-input');
     const taskList = document.getElementById('task-list');
 
